a8fa8cdc35c0377b9e0692f2c1e7a40d52bd8be8
arbiyakhanum <arbiyakhanum.parvam@gmail.com>
2025-03-25 16:33:11 +0530

    first commit

--- EclipseWorkplace.java/CollectionsInJava/src/com/collectionsInJava/CollectionsInJava1.java
+++ EclipseWorkplace.java/CollectionsInJava/src/com/collectionsInJava/CollectionsInJava1.java
@@ -1,5 +1,8 @@
 package com.collectionsInJava;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class CollectionsInJava1 {
 
 	public static void main(String[] args) {
@@ -16,6 +19,16 @@ public class CollectionsInJava1 {
 		 * Root interface in collection framework
 		 * This will provide some of built-in methods to work with any collection class
 		 */
+		
+		Collection collection= new ArrayList();
+		collection.add(10);
+		collection.add("Name");
+		collection.add(32.3);
+		collection.remove(10);
+//		collection.clear();
+		
+		System.out.println(collection);
+		
 
 	}
 
---
+++ EclipseWorkplace.java/CollectionsInJava/src/com/collectionsInJava/ListInJava.java
@@ -0,0 +1,18 @@
+package com.collectionsInJava;
+
+public class ListInJava {
+
+	public static void main(String[] args) {
+		/*
+		 * List:
+		 * --> It is a child interrface of collection
+		 * --> It will contain some of the built in methods that used inside a 
+		 * list implemented class only
+		 * --> When you represent a grpu[ of individual object as a single entity where
+		 * order is preserved and index based access and duplicate is allowed
+		 * 
+		 * 
+		 */
+	}
+
+}
