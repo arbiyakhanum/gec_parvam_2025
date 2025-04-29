e77278ad8709765157636b1915b73fb3ebcf81be
arbiyakhanum <arbiyakhanum.parvam@gmail.com>
2025-04-29 10:48:39 +0530

    first message

--- EclipseWorkplace.java/springSecurityDemo3/src/main/java/com/springSecurityDemo/springSecurityDemo/userconfig/UserConfig.java
+++ EclipseWorkplace.java/springSecurityDemo3/src/main/java/com/springSecurityDemo/springSecurityDemo/userconfig/UserConfig.java
@@ -1,7 +1,5 @@
 package com.springSecurityDemo.springSecurityDemo.userconfig;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetails;
